84f8108522181df200f1dddc80709cebc6e96d46
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-01-12 06:12:20 +0530

    Delete products from HTML when clicking delete

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -17,7 +17,8 @@ cart.forEach((cartItem) => {
   });
   cartSummaryHTML +=
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -102,6 +103,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
      
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   }); 
 });
 
