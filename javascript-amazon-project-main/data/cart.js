ee95c6a0d48e7dcd773177e473199e5750740b68
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-02-21 14:49:36 +0530

    Create automated tests for orderSummary

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -8,7 +8,7 @@ export function loadFromStorage() {
   if (!cart) {
     cart = [{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 1,
+      quantity: 2,
       deliveryOptionId: '1'
     },
     {
