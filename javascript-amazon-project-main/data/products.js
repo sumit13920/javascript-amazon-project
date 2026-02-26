f7ee8db4483dce2d46164bb2aaee3fd1bd419c54
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-02-27 02:30:58 +0530

    Use inheritance and polymorphism to add size chart.

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -33,7 +33,30 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+    Size Chart
+    </a>
+    `;
+  }
+}
+ 
 
 export const products = [
   {
@@ -695,5 +718,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new clothing(productDetails);
+  }
   return new Product(productDetails);
 });
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart } from '../data/cart.js';
+import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -42,6 +42,8 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
+          
+           ${product.extraInfoHTML()}
 
           <div class="product-spacer"></div>
 
@@ -59,16 +61,14 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-
-
 function updateCartQuantity() {
-   let cartQuantity = 0;
+  let cartQuantity = 0;
 
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 };
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
@@ -76,7 +76,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     const productId = button.dataset.productId;
     addToCart(productId);
     updateCartQuantity();
-   
+
   });
 });
- 
+
