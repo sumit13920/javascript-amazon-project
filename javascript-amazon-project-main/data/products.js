9ada013770bb47c1c93d117451f141652fe360bc
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-02-28 23:52:58 +0530

    Load products from the backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -82,7 +82,29 @@ class clothing extends Product {
 // };
 // object3.method();  
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products'); 
+    
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -748,3 +770,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
