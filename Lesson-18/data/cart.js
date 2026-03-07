d409eef6f85f88acb79fafd3d1e1217af19b266e
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-03-07 14:15:09 +0530

    console messages removed.

--- Lesson-18/data/cart.js
+++ Lesson-18/data/cart.js
@@ -100,7 +100,6 @@ export function loadCart(fun) {
 export async function loadCartFetch() {
   const response = await fetch('https://supersimplebackend.dev/cart');
   const text = await response.text();
-  console.log(text);
   return text;
 }
 
--- Lesson-18/data/products.js
+++ Lesson-18/data/products.js
@@ -102,7 +102,6 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
 
-    console.log('load products');
    }).catch((error) => {
 
     console.log('Unexpected error. Please try again later.');
