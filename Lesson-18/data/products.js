f05e732a1c47bb0e3be7d16994f93edde5b2796f
Sumit Kumar Thakur <sumitthakur6500@gmail.com>
2026-03-07 12:49:55 +0530

    Exercise 18q. ans=>Improve the search feature from 18p . Make the search case-insensitive (capitals don't matter).

--- Lesson-18/data/products.js
+++ Lesson-18/data/products.js
@@ -18,12 +18,15 @@ class Product {
   name;
   rating;
   priceCents;
+  keywords;
+
   constructor(productDetails) {
     this.id = productDetails.id;
     this.image = productDetails.image;
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
   }
 
   getStarsUrl() {
--- Lesson-18/scripts/amazon.js
+++ Lesson-18/scripts/amazon.js
@@ -17,7 +17,16 @@ function renderProductsGrid() {
   // filter the products that match the search.
   if (search) {
     filteredProducts = products.filter((product) => {
-      return product.name.includes(search);
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if (keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword ||
+        product.name.toLowerCase().includes(search.toLowerCase());
     });
   }
 
